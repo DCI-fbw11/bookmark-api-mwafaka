2798442bf7022b80c9294c19938bfc54012b8b21
Oliver Weber <oliver@mondough.io>
2019-04-09 15:00:34 +0200

    comments

--- routes/api.js
+++ routes/api.js
@@ -13,22 +13,30 @@ const {
   getBookmarkByID,
   postBookmark
 } = require("../controller/bookmark")
+
+// Route Config
 const apiRoutes = {
   allBookmarks: "/bookmarks",
   bookmarkByID: "/bookmarks/:id",
   postBookmark: "/bookmarks"
 }
 
+// To show our api users what is possible we can show all endpoints at home route (/)
 router.get("/", (req, res) => {
   res.json({ availableRoutes: Object.values(apiRoutes) })
 })
 
+// GET
 router.get(apiRoutes.allBookmarks, getBookmarks)
 router.get(apiRoutes.bookmarkByID, getBookmarkByID)
 
+// POST
 router.post(apiRoutes.postBookmark, postBookmark)
+
+// The middleware that actually sends the response
 router.use(sendJsonResp)
 
+// Custom error handler
 router.use(apiErrorMiddleware)
 
 module.exports = router
