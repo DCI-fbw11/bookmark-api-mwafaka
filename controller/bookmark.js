d214d2d1bd2accfc52b382c49fcc2f7e0707e977
mwafak <Mwafaqarbash3@gmail.com>
2019-04-10 13:37:01 +0200

    deletBookmark

--- controller/bookmark.js
+++ controller/bookmark.js
@@ -1,54 +1,68 @@
-const db = require("../db")
-const uuidv1 = require("uuid/v1")
+const db = require("../db");
+const uuidv1 = require("uuid/v1");
 
 module.exports = {
   getBookmarks: (req, res, next) => {
     res.locals.response = Object.assign({}, res.locals.response || {}, {
       bookmark: db.get("bookmarks").value()
-    })
+    });
 
-    next()
+    next();
   },
 
   getBookmarkByID: (req, res, next) => {
-    const { id } = req.params
+    const { id } = req.params;
 
     if (!id) {
-      throw new Error("No ID defined, bookmarks/:id")
+      throw new Error("No ID defined, bookmarks/:id");
     }
 
     const bookmark = db
       .get("bookmarks")
       .find({ id })
-      .value()
+      .value();
 
     if (!bookmark) {
-      throw new Error("No bookmark found for id")
+      throw new Error("No bookmark found for id");
     }
 
     res.locals.response = Object.assign({}, res.locals.response || {}, {
       bookmark
-    })
+    });
 
-    next()
+    next();
   },
 
   postBookmark: (req, res, next) => {
-    const { url, tags } = req.body
+    const { url, tags } = req.body;
 
     if (!url) {
-      throw new Error("No url to bookmark defined")
+      throw new Error("No url to bookmark defined");
     }
 
-    const newBookmark = { id: uuidv1(), url: url, createdAt: Date.now(), tags }
+    const newBookmark = { id: uuidv1(), url: url, createdAt: Date.now(), tags };
     db.get("bookmarks")
       .push(newBookmark)
-      .write()
+      .write();
 
     res.locals.response = Object.assign({}, res.locals.response || {}, {
       bookmark: newBookmark
-    })
+    });
 
-    next()
+    next();
+  },
+
+  deleteBookmark: (req, res, next) => {
+    const { id } = req.params;
+    const bookmark = db
+      .get("bookmarks")
+
+      .remove({ id: id })
+      .write();
+
+    res.locals.response = Object.assign({}, res.locals.response || {}, {
+      bookmark
+    });
+    next();
   }
-}
+};
--- routes/api.js
+++ routes/api.js
@@ -1,42 +1,47 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 // Middleware
-const { apiErrorMiddleware } = require("../middleware/api")
+const { apiErrorMiddleware } = require("../middleware/api");
 
 //Helper
-const { sendJsonResp } = require("../helper")
+const { sendJsonResp } = require("../helper");
 
 // Controller
 const {
   getBookmarks,
   getBookmarkByID,
-  postBookmark
-} = require("../controller/bookmark")
+  postBookmark,
+  deleteBookmark
+} = require("../controller/bookmark");
 
 // Route Config
 const apiRoutes = {
   allBookmarks: "/bookmarks",
   bookmarkByID: "/bookmarks/:id",
-  postBookmark: "/bookmarks"
-}
+  postBookmark: "/bookmarks",
+  deleteBookmark: "/bookmarks/:id"
+};
 
 // To show our api users what is possible we can show all endpoints at home route (/)
 router.get("/", (req, res) => {
-  res.json({ availableRoutes: Object.values(apiRoutes) })
-})
+  res.json({ availableRoutes: Object.values(apiRoutes) });
+});
 
 // GET
-router.get(apiRoutes.allBookmarks, getBookmarks)
-router.get(apiRoutes.bookmarkByID, getBookmarkByID)
+router.get(apiRoutes.allBookmarks, getBookmarks);
+router.get(apiRoutes.bookmarkByID, getBookmarkByID);
 
 // POST
-router.post(apiRoutes.postBookmark, postBookmark)
+router.post(apiRoutes.postBookmark, postBookmark);
+
+//Delete
+router.delete(apiRoutes.deleteBookmark, deleteBookmark);
 
 // The middleware that actually sends the response
-router.use(sendJsonResp)
+router.use(sendJsonResp);
 
 // Custom error handler
-router.use(apiErrorMiddleware)
+router.use(apiErrorMiddleware);
 
-module.exports = router
+module.exports = router;
